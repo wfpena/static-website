1dd55a2179038c33125e84de10751eb8cb0c32e4
Wilson Pena <w.fernando76@hotmail.com>
2019-01-16 16:14:44 -0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/'));
+//app.use(express.static(__dirname + '/'));
 
 app.get('/*', function(req,res) {
 res.send(path.join(__dirname + '/test.html'));
